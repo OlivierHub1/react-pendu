2fe4fba09aa14a8b7df80e1d4cf8a12a2570de84
Olivier <olivier18102@gmail.com>
2023-07-21 18:03:49 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {lettresList1, lettresList2, lettresList3, mots } from "./Pendu";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="section container mt-5">
+        <h1 className="text-center text-light user-select-none">BONHOMME PENDU</h1>
+        <div className="row my-4">
+          <div className="col">
+            <img className="mx-auto d-block rounded" src="src/assets/img/pendu00.jpg" alt="pendu" />
+          </div>
+        </div>
+        <div className="row my-4 justify-content-center">
+          <div className="col-4 bg-light rounded">
+            <h2 className="text-center text-danger user-select-none">_ _ _ _ _</h2>
+          </div>
+        </div>
+       
+      </section>
+      <section className="container">
+        <div className="row">
+          <div className="btn-group my-1">
+            {lettresList1.map((lettre) => (
+                <button type="button" className="btn btn-success my-1" key={lettre}>
+                {lettre}
+              </button>
+            ))}
+        </div>
+        </div>
+        <div className="row">
+          <div className="btn-group my-1">
+            {lettresList2.map((lettre) => (
+                <button type="button" className="btn btn-success my-1" key={lettre}>
+                {lettre}
+              </button>
+            ))}
+        </div>
+        </div>
+        <div className="row">
+          <div className="btn-group my-1 justify-content-center">
+            {lettresList3.map((lettre) => (
+                <button type="button" className="btn btn-success my-1" key={lettre}>
+                {lettre}
+              </button>
+            ))}
+        </div>
+        </div>
+         
+        
+        
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
