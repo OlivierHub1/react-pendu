71e77b5f290af96ab2ad56978924b41fa71eb005
Olivier <olivier18102@gmail.com>
2023-08-11 18:12:08 -0400

    Update

--- src/Pendu.tsx
+++ src/Pendu.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const lettresList1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 export const lettresList2 = ["K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"];
 export const lettresList3 = ["U", "V", "W", "X", "Y", "Z"];
