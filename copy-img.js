537fc25cc6a50ec51af8915cc4cfff47481d59ce
Olivier <olivier18102@gmail.com>
2023-08-11 19:51:10 -0400

    Update

---
+++ copy-img.js
@@ -0,0 +1,14 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const buildDirectory = path.join(__dirname, 'build'); // Change to 'dist' if that's your output directory
+const imgSourceDirectory = path.join(__dirname, 'src', 'assets', 'img');
+const imgDestinationDirectory = path.join(buildDirectory, 'assets', 'img');
+
+// Ensure the destination directory exists
+fs.ensureDirSync(imgDestinationDirectory);
+
+// Copy the contents of the img folder to the build output directory
+fs.copySync(imgSourceDirectory, imgDestinationDirectory);
+
+console.log('Images copied to build directory.');
