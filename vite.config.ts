968987e91443529a4d555d7c6c7dcd2ef36c4152
Olivier <olivier18102@gmail.com>
2023-08-11 20:22:24 -0400

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/react-pendu/",
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][ext]',
+      },
+    },
+  },
+});
